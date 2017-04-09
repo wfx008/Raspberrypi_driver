a12426a753b01087e2d6e6a02a6bfab90970228e
wfx008 <2325948895@qq.com>
2017-04-10 00:37:51 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("wfx");
+MODULE_DESCRIPTION("This is test module !!!");
 void hello_init(void)
 {
         printk(KERN_INFO"This is hello module test !!!\n");
